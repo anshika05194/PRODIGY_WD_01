af6cfd223a9765993e27dd4ae1c0913d1d51d43f
Anshika Singh <anshikasingh202389@gmail.com>
2024-08-20 16:04:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('.cta').addEventListener('click', () => {
+    alert('Get started now!');
+});
+
+document.querySelector('.hero-btn').addEventListener('click', () => {
+    window.location.href = '#about';
+});
+document.querySelector('form button').addEventListener('click',()=>{
+    alert('Received your message');
+});
